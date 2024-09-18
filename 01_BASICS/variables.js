f444e79e229cfca970820ee375dcd219fc5448d4
prafullkulli <85009217+prafullkulli@users.noreply.github.com>
2024-09-18 12:06:46 +0000

    container config done

---
+++ 01_BASICS/variables.js
@@ -0,0 +1,26 @@
+const accountId = 123456;
+let accountEmail = "prafullk95@gmail.com";
+var accountPassword = "manipur";
+accountCity = "Japan";
+let accountState=null ;
+var toNumber = Number(accountId)
+
+// accountId = 123
+pk=[1,2,3]
+// console.log(accountId);
+// console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+// console.log(typeof toNumber)
+// console.log(accountId)
+let j=123
+let k="456"
+let yes="1"
+//To convert data type
+//number to string i.e let 
+let numberToString = String(j)
+console.log(numberToString)
+// string to number i.e, let 
+let stringToNumber=Number(k)
+console.log(k)
+// To boolean i.e 
+let toBoolean=Boolean(yes)
+console.log(toBoolean)
